31f6c0e03e83458c3777ea5e278e4d15a9198c2b
Vanderson Erlan <vandersonerlan@Vandersons-MBP.bbrouter>
2024-03-29 01:53:45 -0300

    FEAT: using Db class variables to connect and find registers with new route registros

--- src/app.js
+++ src/app.js
@@ -1,1 +1,37 @@
- 
+const express = require('express')
+const cors = require('cors')
+const Db = require('./db.js')
+
+
+const app = express()
+const PORT = 3000
+const db = new Db()
+
+app.use(cors({
+    allowedHeaders: ['Content-Type'],  
+}));
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('API rodando! 🥳')
+})
+
+app.get('/registros', async (req, res) => {
+    try {
+        const cursor = db.registros.find({})
+        const registros_achados = await cursor.toArray()
+        res.json(registros_achados)
+    }
+
+    catch(error) {
+        console.log('Error fetching registers', error)
+        res.status(500).send()
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`API listening on PORT ${PORT} `)
+})
+
+module.exports = app
