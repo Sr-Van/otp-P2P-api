68bff34e49e5dd4ddde3d8bb58f2d77aefb85316
Vanderson Erlan <vandersonerlan@Vandersons-MBP.bbrouter>
2024-03-30 04:44:24 -0300

    FIX: removing code from app and leting clean routes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const cors = require('cors')
-const Db = require('./db.js')
-
+const controller = require('./controllers/controller.js')
 
 const app = express()
 const PORT = 3000
-const db = new Db()
+
 
 app.use(cors({
     allowedHeaders: ['Content-Type'],  
@@ -17,29 +16,8 @@ app.get('/', (req, res) => {
     res.send('API rodando! 🥳')
 })
 
-app.get('/registros', async (req, res) => {
-    try {
-        const cursor = db.registros.find({})
-        const registros_achados = await cursor.toArray()
-        res.json(registros_achados)
-    }
-
-    catch(error) {
-        res.status(500).send()
-    }
-})
-
-app.post('/add-registro', async (req, res) => {
-    try {
-        const doc = req.body
-        await db.registros.insertOne(doc);
-    
-        res.json({ "insert sended": "true" ,
-                  "doc": doc});
-      } catch (error) {
-        res.status(500).send('Internal Server Error' + error);
-      }
-})
+app.get('/registros', controller.getAll)
+app.post('/add-registro', controller.addRegistro)
 
 app.listen(PORT, () => {
     console.log(`API listening on PORT ${PORT} `)
