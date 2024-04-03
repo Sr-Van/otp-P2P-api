f552ee8dd1fa729e55eae0e633d33a508420b350
Vanderson Erlan <vandersonerlan@Vandersons-MBP.bbrouter>
2024-04-03 14:56:15 -0300

    FIX: app.js file change name to index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const controller = require('./src/controllers/controller.js')
+const controller = require('../src/controllers/controller.js')
 
 const app = express()
 const PORT = 3000
